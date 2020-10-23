ea0888b2f27f3e0cc4bdad45c40131c2180891cb
Adam McKee <adam.be.g84d@gmail.com>
2020-10-23 13:17:43 -0500

    api updates and adding docs

---
+++ README.md
@@ -0,0 +1,56 @@
+## SSE Upgrade
+
+This library exports a func that upgrades http requests to an event streaming connection by sending a `Content-Type: text/event-stream` header to the client.
+```go
+http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    var connection *sse.Connection
+    connection, _ = Upgrade(w, r)
+    connection.SendString("thanks for connecting. here's some data.")
+})
+```
+
+Connection's `SendBytes([]byte)`, `SendString(string)` and `SendJson(interface{})` funcs will format and send data to the client.
+
+Use `Close()` when you're done streaming event data.
+
+A connection's BuildMessage() func can be used to send a payload with the id and event attributes.
+```go
+connection.BuildMessage().WithId("id").WithEvent("event").SendString("data")
+```
+This message will be sent to the client:
+```text
+id: id
+event: event
+data: data
+
+```
+
+### Example
+```go
+package main
+
+import (
+	"github.com/eighty4/sse"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	connection, err := sse.Upgrade(w, r)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Println("sse upgrade error", err.Error())
+		return
+	}
+	connection.BuildMessage().WithEvent("auth").SendString("token")
+	connection.Close()
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+```
+
+#### Original Repository
+I found originating source for sse.go on GitHub a couple of years ago, but I couldn't find the repository to reference when publishing updates.
--- sse.go
+++ sse.go
@@ -1,38 +1,54 @@
+// Package sse provides a utility for upgrading http requests to event streaming connections
+// with server sent events (SSE).
 package sse
 
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+// Connection provides channels for sending event messages, closing the connection and
+// receiving errors from writing to the http response
 type Connection struct {
-	messages chan Message
-	shutdown chan bool
+	errors   <-chan error
+	messages chan<- Message
+	shutdown chan<- bool
 	isOpen   bool
 }
 
+// BuildMessage returns a MessageBuilder, a fluent-style builder api for sending events
 func (connection *Connection) BuildMessage() *MessageBuilder {
-	return &MessageBuilder{&Message{}, connection}
+	return &MessageBuilder{
+		message:    &Message{},
+		connection: connection,
+	}
 }
 
+// SendBytes sends a series of bytes for an event's data without an id or event field
 func (connection *Connection) SendBytes(data []byte) error {
 	return connection.BuildMessage().SendBytes(data)
 }
 
+// SendString sends a string for an event's data without an id or event field
 func (connection *Connection) SendString(data string) error {
 	return connection.BuildMessage().SendString(data)
 }
 
+// SendJson marshals data into a json string for an event's data and sends it
+// without an id or event field
 func (connection *Connection) SendJson(data interface{}) error {
 	return connection.BuildMessage().SendJson(data)
 }
 
+// IsOpen returns whether connection is still open for sending event data
 func (connection *Connection) IsOpen() bool {
 	return connection.isOpen
 }
 
+// Close sends a shutdown signal to close the connection for streaming data
 func (connection *Connection) Close() {
 	connection.shutdown <- true
 }
@@ -45,46 +61,55 @@ func (connection *Connection) send(message *Message) error {
 	return nil
 }
 
+// MessageBuilder is a fluent-style builder api for sending events
 type MessageBuilder struct {
 	message    *Message
 	connection *Connection
 }
 
+// WithId adds an id attribute to event data
 func (messageBuilder *MessageBuilder) WithId(id string) *MessageBuilder {
 	messageBuilder.message.id = id
 	return messageBuilder
 }
 
+// WithEvent adds an event attribute to event data
 func (messageBuilder *MessageBuilder) WithEvent(event string) *MessageBuilder {
 	messageBuilder.message.event = event
 	return messageBuilder
 }
 
+// SendBytes sends a series of bytes with the specified id and event attributes
 func (messageBuilder *MessageBuilder) SendBytes(data []byte) error {
 	messageBuilder.message.data = data
 	return messageBuilder.connection.send(messageBuilder.message)
 }
 
+// SendBytes sends a string with the specified id and event attributes
 func (messageBuilder *MessageBuilder) SendString(data string) error {
 	messageBuilder.message.data = []byte(data)
 	return messageBuilder.connection.send(messageBuilder.message)
 }
 
+// SendJson marshals data into a json string and sends it without an id or event field
 func (messageBuilder *MessageBuilder) SendJson(data interface{}) error {
 	if data, err := json.Marshal(data); err != nil {
 		return err
 	} else {
-		messageBuilder.message.data = []byte(data)
+		messageBuilder.message.data = data
 		return messageBuilder.connection.send(messageBuilder.message)
 	}
 }
 
+// Message contains id, event and data attributes of an event message
 type Message struct {
 	id    string
 	event string
 	data  []byte
 }
 
+// Upgrade sends headers to client to upgrade the request to an SSE connection and
+// returns a Connection handle for sending messages.
 func Upgrade(writer http.ResponseWriter, request *http.Request) (*Connection, error) {
 
 	flusher, ok := writer.(http.Flusher)
@@ -92,9 +117,13 @@ func Upgrade(writer http.ResponseWriter, request *http.Request) (*Connection, er
 		return nil, errors.New("streaming not supported")
 	}
 
+	errorChannel := make(chan error)
+	messageChannel := make(chan Message)
+	shutdownChannel := make(chan bool)
 	sseConnection := &Connection{
-		messages: make(chan Message),
-		shutdown: make(chan bool),
+		errors:   errorChannel,
+		messages: messageChannel,
+		shutdown: shutdownChannel,
 		isOpen:   true,
 	}
 
@@ -102,19 +131,34 @@ func Upgrade(writer http.ResponseWriter, request *http.Request) (*Connection, er
 	writer.Header().Set("Cache-Control", "no-cache")
 	writer.Header().Set("Connection", "keep-alive")
 
+	handleError := func(err error) {
+		if err != nil {
+			select {
+			case errorChannel <- err:
+				break
+			default:
+				log.Println("sse write error: " + err.Error())
+			}
+		}
+	}
+
 	go func() {
 		for {
+			var err error
 			select {
-			case message := <-sseConnection.messages:
+			case message := <-messageChannel:
 				if len(message.id) > 0 {
-					fmt.Fprintf(writer, "id: %s\n", message.id)
+					_, err = fmt.Fprintf(writer, "id: %s\n", message.id)
+					handleError(err)
 				}
 				if len(message.event) > 0 {
-					fmt.Fprintf(writer, "event: %s\n", message.event)
+					_, err = fmt.Fprintf(writer, "event: %s\n", message.event)
+					handleError(err)
 				}
-				fmt.Fprintf(writer, "data: %s\n\n", message.data)
+				_, err = fmt.Fprintf(writer, "data: %s\n\n", message.data)
+				handleError(err)
 				flusher.Flush()
-			case <-sseConnection.shutdown:
+			case <-shutdownChannel:
 			case <-request.Context().Done():
 				sseConnection.isOpen = false
 				return
